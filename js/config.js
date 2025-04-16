ba9486ece14c28e1b0e288bb7721523039d5d3e8
Akash.V <55589528+akasah@users.noreply.github.com>
2025-04-16 13:00:10 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_E7XBrxB4s', // ap-south-1_E7XBrxB4s
+        userPoolClientId: '7bc1gkohtfoora0e5ug3bp5p45', // 7bc1gkohtfoora0e5ug3bp5p45
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
