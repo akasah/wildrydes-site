0e40b7801f9abf8c5e67ea2e77055f2d0e781b35
Akash.V <55589528+akasah@users.noreply.github.com>
2025-04-16 13:25:36 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_E7XBrxB4s', // ap-south-1_E7XBrxB4s
-        userPoolClientId: '7bc1gkohtfoora0e5ug3bp5p45', // 7bc1gkohtfoora0e5ug3bp5p45
+        userPoolId: 'ap-south-1_TkMKhHFjq', // ap-south-1_E7XBrxB4s
+        userPoolClientId: '27p1h6fkrup5mjonuu1f5qmoed', // 7bc1gkohtfoora0e5ug3bp5p45
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
